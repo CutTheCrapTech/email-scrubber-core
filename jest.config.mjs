963795fa820af8e68f2311826696781a7ac49957
Karteek <120569182+karteekiitg@users.noreply.github.com>
2025-06-20 13:38:29 +0530

    updated jest config
    Signed-off-by: Karteek <120569182+karteekiitg@users.noreply.github.com>

---
+++ jest.config.mjs
@@ -0,0 +1,27 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: './tsconfig.json',
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  testMatch: ['**/src/**/*.test.ts'],
+  testEnvironment: 'node',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(linkedom|jsdom)/)',
+  ],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts', '!src/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+};
